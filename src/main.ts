598fefde637d71bcde621ed05339be887fc2d448
Technote <technote.space@gmail.com>
2019-11-23 18:53:50 +0900

    chore: tweaks

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,25 @@ import path from 'path';
 import { setFailed, getInput } from '@actions/core';
 import { context, GitHub } from '@actions/github';
 import { isTargetEvent } from '@technote-space/filter-github-action';
-import { Logger, Utils } from '@technote-space/github-action-helper';
+import { Logger, ContextHelper } from '@technote-space/github-action-helper';
 import { updatePackageVersion, commit } from './utils/package';
 import { TARGET_EVENTS } from './constant';
-
-const {showActionInfo} = Utils;
 
 /**
  * run
  */
 async function run(): Promise<void> {
-	try {
-		const logger = new Logger();
-		showActionInfo(path.resolve(__dirname, '..'), logger, context);
+	const logger = new Logger();
+	ContextHelper.showActionInfo(path.resolve(__dirname, '..'), logger, context);
 
-		if (!isTargetEvent(TARGET_EVENTS, context)) {
-			logger.info('This is not target event.');
-			return;
-		}
+	if (!isTargetEvent(TARGET_EVENTS, context)) {
+		logger.info('This is not target event.');
+		return;
+	}
 
-		if (await updatePackageVersion(context)) {
-			await commit(new GitHub(getInput('GITHUB_TOKEN', {required: true})), context);
-		}
-	} catch (error) {
-		setFailed(error.message);
+	if (await updatePackageVersion(context)) {
+		await commit(new GitHub(getInput('GITHUB_TOKEN', {required: true})), context);
 	}
 }
 
-run();
+run().catch(error => setFailed(error.message));
--- src/utils/command.ts
+++ src/utils/command.ts
@@ -4,7 +4,7 @@ const command = new Command(new Logger());
 const {execAsync} = command;
 
 export const getBranchesByTag = async(tagName: string): Promise<string[]> => {
-	return (await execAsync({command: `git branch -a --contains ${tagName} | cut -b 3-`}))
+	return (await execAsync({command: `git branch -a --contains ${tagName} | cut -b 3-`})).stdout
 		.trim()
 		.split(/\r?\n/)
 		.filter(item => item)
