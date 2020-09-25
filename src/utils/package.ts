55020066b93238e81c20eda05b2c15b02bc25880
Technote <technote.space@gmail.com>
2020-09-25 16:44:11 +0900

    feat: use log helper

--- src/utils/package.ts
+++ src/utils/package.ts
@@ -2,7 +2,8 @@ import fs from 'fs';
 import {setOutput} from '@actions/core';
 import {Context} from '@actions/github/lib/context';
 import {Octokit} from '@technote-space/github-action-helper/dist/types';
-import {ApiHelper, Logger, ContextHelper} from '@technote-space/github-action-helper';
+import {ApiHelper, ContextHelper} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {replaceInFile} from 'replace-in-file';
 import {
   getPackageDir,
